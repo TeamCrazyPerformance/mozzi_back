62191dc307647868aedd2e5a45e5a9aa2dc6e25c
KUvH <kuvh@live.co.kr>
2019-03-06 01:59:25 +0900

    [Add] User Service and Impl

---
+++ src/main/java/com/tcp/mozzi/back/service/user/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package com.tcp.mozzi.back.service.user;
+
+import com.tcp.mozzi.back.domain.user.User;
+import com.tcp.mozzi.back.mapper.user.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    private UserMapper userMapper;
+
+    @Autowired
+    public void setUserMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public User getUserById(Integer id) {
+        return userMapper.selectUserById(id);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userMapper.selectUsers();
+    }
+
+    @Override
+    public void addUser(User user) {
+        userMapper.insertUser(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userMapper.updateUser(user);
+    }
+
+    @Override
+    public void deleteUserById(Integer id) {
+        userMapper.deleteUserById(id);
+    }
+}
