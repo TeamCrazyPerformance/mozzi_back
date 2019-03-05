75cede862ddd3de5a538675b57043daf6f9dbb3e
KUvH <kuvh@live.co.kr>
2019-03-06 01:34:35 +0900

    [Add] User Mapper Interface

---
+++ src/main/java/com/tcp/mozzi/back/mapper/user/UserMapper.java
@@ -0,0 +1,17 @@
+package com.tcp.mozzi.back.mapper.user;
+
+import com.tcp.mozzi.back.domain.user.User;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface UserMapper {
+    User selectUserById(Integer id);
+    List<User> selectUsers();
+    void insertUser(User user);
+    void updateUser(User user);
+    void deleteUserById(Integer id);
+}
