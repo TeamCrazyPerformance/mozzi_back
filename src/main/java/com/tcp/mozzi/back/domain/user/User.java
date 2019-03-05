54c3bcd165889311f5998bc001c87d8b63b136ed
KUvH <kuvh@live.co.kr>
2019-03-06 01:27:20 +0900

    [Add] User Entity Class

---
+++ src/main/java/com/tcp/mozzi/back/domain/user/User.java
@@ -0,0 +1,37 @@
+package com.tcp.mozzi.back.domain.user;
+
+import com.tcp.mozzi.back.domain.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id;
+    private String name;
+    private String password;
+    private UserStatus status;
+    private String nickname;
+    private String studentNum;
+    private String phoneNum;
+    private String email;
+    private Date birthday;
+
+    public enum UserStatus {
+        OK,
+        WAIT
+    }
+}
