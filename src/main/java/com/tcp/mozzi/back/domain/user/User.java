bf72361ec53c9074d0bcc9aea10df4b773cbdb92
KUvH <kuvh@live.co.kr>
2019-03-07 05:52:53 +0900

    [Fix Feature] Remove JPA Annotations, Add now() function in insert and update query for create_at modify_at columns

--- src/main/java/com/tcp/mozzi/back/domain/user/User.java
+++ src/main/java/com/tcp/mozzi/back/domain/user/User.java
@@ -1,25 +1,17 @@
 package com.tcp.mozzi.back.domain.user;
 
-import com.tcp.mozzi.back.domain.BaseTimeEntity;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.util.Date;
 
 @Getter
 @Setter
-@Entity
 @Builder
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class User extends BaseTimeEntity {
+public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String password;
