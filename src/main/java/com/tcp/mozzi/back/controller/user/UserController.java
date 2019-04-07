a300d08c99bed293eb58cee5e965af672f315749
KUvH <kuvh@live.co.kr>
2019-04-08 05:16:34 +0900

    [Update Feature] Remove Unused Import

--- src/main/java/com/tcp/mozzi/back/controller/user/UserController.java
+++ src/main/java/com/tcp/mozzi/back/controller/user/UserController.java
@@ -5,11 +5,9 @@ import com.tcp.mozzi.back.dto.user.RegisterUserRequestDto;
 import com.tcp.mozzi.back.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.text.ParseException;
 
 @RequestMapping("/user")
 @RestController
@@ -21,7 +19,7 @@ public class UserController {
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    public DefaultApiMessageDto registerUser(@Valid @RequestBody RegisterUserRequestDto userDto) throws ParseException, BindException {
+    public DefaultApiMessageDto registerUser(@Valid @RequestBody RegisterUserRequestDto userDto) {
         userService.addUser(userDto.toEntity());
 
         return new DefaultApiMessageDto();
