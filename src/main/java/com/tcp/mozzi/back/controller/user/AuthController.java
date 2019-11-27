3bc6f8c5fd1caa9bedcad23c3164a5ff37df482c
ruru14 <ljw2889@gmail.com>
2019-11-27 21:53:29 +0900

    [Update] Log (ing)

--- src/main/java/com/tcp/mozzi/back/controller/user/AuthController.java
+++ src/main/java/com/tcp/mozzi/back/controller/user/AuthController.java
@@ -61,6 +61,8 @@ public class AuthController {
                 "createAuthenticationToken",
                 loginRequestDto.toString()));
 
+        //TODO : get request body
+
 
         return new ResponseEntity<>(new UserTokenResponseDto(token, jwtTokenUtil.getRoleFromToken(token)), HttpStatus.OK);
     }
