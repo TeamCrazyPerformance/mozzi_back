7c5c1a313fb85d31233b8859758768095d1be280
ruru14 <ljw2889@gmail.com>
2020-01-01 13:58:27 +0900

    [Update] Exam controller api description

--- src/main/java/com/tcp/mozzi/back/controller/exam/ExamController.java
+++ src/main/java/com/tcp/mozzi/back/controller/exam/ExamController.java
@@ -4,23 +4,23 @@ import com.tcp.mozzi.back.domain.exam.Exam;
 import com.tcp.mozzi.back.dto.exam.CreateExamDto;
 import com.tcp.mozzi.back.dto.exam.GetExamListDto;
 import com.tcp.mozzi.back.service.exam.ExamService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.yaml.snakeyaml.reader.StreamReader;
 
 @RequestMapping("/exam")
+@Api(tags = "Exam", description = "기출문제 (족보)")
 @RestController
 public class ExamController {
 
     @Autowired
     private ExamService examService;
 
-    ExamController(ExamService examService){
-        this.examService = examService;
-    }
-
     @RequestMapping(value = "", method = RequestMethod.GET)
     @ResponseBody
+    @ApiOperation(value = "문제 보기", notes = "등록되어 있는 기출문제를 불러옵니다.")
     public GetExamListDto getList(
             @RequestParam(value = "page", required = false, defaultValue = "1") int page,
             @RequestParam(value = "limit", required = false, defaultValue = "10") int limit){
@@ -30,21 +30,25 @@ public class ExamController {
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
+    @ApiOperation(value = "문제 등록", notes = "기출문제를 등록합니다.")
     public void createExam(@RequestBody CreateExamDto exam){
 
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @ApiOperation(value = "문제 수정", notes = "지정된 기출문제를 수정합니다.")
     public void modifyExam(){
 
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @ApiOperation(value = "문제 삭제", notes = "지정된 기출문제를 삭제합니다.")
     public void deleteExam(){
 
     }
 
     @RequestMapping(value = "/search", method = RequestMethod.GET)
+    @ApiOperation(value = "문제 탐색", notes = "기출문제를 탐색합니다.")
     public void searchExam(){
 
     }
